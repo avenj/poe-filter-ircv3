af576cb93c2689d602812e71e816cc99fbc3efa0
Jon Portnoy <avenj@cobaltirc.org>
2013-06-08 18:58:38 -0400

    Skip extraneous spaces before trailing args.
    	modified:   lib/POE/Filter/IRCv3.pm

--- lib/POE/Filter/IRCv3.pm
+++ lib/POE/Filter/IRCv3.pm
@@ -200,7 +200,8 @@ sub _parseline {
       last PARAM
     } else {
       push @{ $event{params} }, substr $remains, 0, $space;
-      $remains = substr $remains, ($space + 1)
+      $remains = substr $remains, ($space + 1);
+      $remains = substr($remains, 1) while index($remains, SPCHR) == 0
     }
   }
 
