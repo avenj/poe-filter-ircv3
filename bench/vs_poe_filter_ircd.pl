8f8238c2348ccf5eb96ba26674cea1e7592f521b
Jon Portnoy <avenj@cobaltirc.org>
2013-06-09 05:50:02 -0400

    bench multiparam strs

--- bench/vs_poe_filter_ircd.pl
+++ bench/vs_poe_filter_ircd.pl
@@ -10,17 +10,21 @@ my $basic = ':test!me@test.ing PRIVMSG #Test :This is a test'
             .' but not of the emergency broadcast system'
             .' foo bar baz quux snarf';
 
-my ($ev_old_one, $ev_old_two);
-my ($ev_new_one, $ev_new_two);
+my $multiparam = ':test!me@test.ing foo bar baz quux frobulate snack';
+
+my ($ev_old_one, $ev_old_two, $ev_old_three);
+my ($ev_new_one, $ev_new_two, $ev_new_three);
 my $get_tests = +{
   old_get => sub {
-    $ev_old_one = $old->get([ $basic ]);
-    $ev_old_two = $old->get([ 'foo bar' ]);
+    $ev_old_one   = $old->get([ $basic ]);
+    $ev_old_two   = $old->get([ 'foo bar' ]);
+    $ev_old_three = $old->get([ $multiparam ]);
   },
 
   new_get => sub {
-    $ev_new_one = $new->get([ $basic ]);
-    $ev_new_two = $new->get([ 'foo bar' ]);
+    $ev_new_one   = $new->get([ $basic ]);
+    $ev_new_two   = $new->get([ 'foo bar' ]);
+    $ev_new_three = $new->get([ $multiparam ]);
   },
 };
 
@@ -31,11 +35,13 @@ my $put_tests = +{
   old_put => sub {
     my $foo = $old->put([ @$ev_old_one ]);
     my $bar = $old->put([ @$ev_old_two ]);
+    my $baz = $old->put([ @$ev_old_three ]);
   },
 
   new_put => sub {
     $new->put([ @$ev_new_one ]);
     $new->put([ @$ev_new_two ]);
+    $new->put([ @$ev_new_three ]);
   },
 };
 
